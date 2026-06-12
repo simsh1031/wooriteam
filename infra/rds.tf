78f47955c9a774daf5590d7d2c791e01491fdca7
simsh1031 <sshsim1031@gmail.com>
2026-06-12 11:23:28 +0900

    feat: rds 삭제 가능 ver

--- infra/rds.tf
+++ infra/rds.tf
@@ -78,8 +78,8 @@ resource "aws_db_instance" "main" {
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.main.name
 
-  deletion_protection       = true
-  skip_final_snapshot       = false
+  deletion_protection       = false
+  skip_final_snapshot       = true
   final_snapshot_identifier = "${var.project_name}-db-final"
 
   backup_retention_period = 7
